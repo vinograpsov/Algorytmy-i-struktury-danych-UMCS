bebbf79ecc5dc82c4fb54a5f2ee238acb4ca6934
Kiryl <kirvinpost@gmail.com>
2021-11-24 15:11:08 +0300

    temp version

--- pozar/input.cpp
+++ pozar/input.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 struct Point 
@@ -13,6 +14,12 @@ struct ExitPoint
     Point exit;
     int manStepCount;
     int fireStepCount;
+};
+
+struct  queueNode
+{
+    Point pt;
+    int dist; 
 };
 
 
@@ -66,7 +73,6 @@ int makeStepArr(int arrKind, int* arr,int value){
 
 
 
-
 void testInp(int** arr,int rows, int cols){
     for (int i = 0;i < rows;i++){
         for(int j = 0;j < cols;j++){
@@ -78,9 +84,70 @@ void testInp(int** arr,int rows, int cols){
 
 
 
+int rowNum[] = {-1, 0, 0, 1};
+int colNum[] = {0, -1, 1, 0};
+ 
+
+int BFS(int** mat, Point src, Point dest, int ROW, int COL)
+{
+    if (!mat[src.x][src.y] || !mat[dest.x][dest.y])
+        return -1;
+ 
+    bool** visited = new bool* [ROW];
+        for (int i = 0; i < ROW; i++){
+            visited[i] = new bool[COL];;
+        }
+    
+    memset(visited, false, sizeof visited);
+     
+    visited[src.x][src.y] = true;
+
+    queue<queueNode> q;
+     
+    queueNode s = {src, 0};
+    q.push(s); 
+ 
+    while (!q.empty())
+    {
+        queueNode curr = q.front();
+        Point pt = curr.pt;
+ 
+        if (pt.x == dest.x && pt.y == dest.y)
+            return curr.dist;
+ 
+        q.pop();
+ 
+        for (int i = 0; i < 4; i++)
+        {
+            int row = pt.x + rowNum[i];
+            int col = pt.y + colNum[i];
+             
+            if (mat[row][col] && !visited[row][col])
+            {
+                visited[row][col] = true;
+                queueNode Adjcell = { {row, col},curr.dist + 1 };
+                q.push(Adjcell);
+            }
+        }
+    }
+
+    return -1;
+}
+
+// int shortestWay(int** mat, ExitPoint destenation,Point start, int ROW, int COL, bool enterType){
+//     if(enterType == 0){
+//         int shortest = BFS(mat,start,destenation.exit,ROW,COL);
+//     }
+//     else
+//     {
+//         int shortest = BFS(mat,start,destenation.exit,ROW,COL);
+//     }
+// }
+
 
 
 int main(){
+    vector<int> answers;  
     // получаем количество тестов
     int numOfTests;
     cin >> numOfTests;
@@ -125,6 +192,8 @@ int main(){
                     ExitPoint temp;
                     temp.exit.x = j;
                     temp.exit.y = i;
+                    temp.fireStepCount = -1;
+                    temp.manStepCount = -1;
                     exitPoints.insert(exitPoints.end(),temp);
                     exitNum++;
                 }
@@ -132,6 +201,8 @@ int main(){
                     ExitPoint temp;
                     temp.exit.x = j;
                     temp.exit.y = i;
+                    temp.fireStepCount = -1;
+                    temp.manStepCount = -1;
                     exitPoints.insert(exitPoints.end(),temp);
                     exitNum++;
                 }
@@ -157,17 +228,33 @@ int main(){
                     maze[i][j] = 1;
                 }                 
                 
-
+                
                 
             }
             
         }
-        testInp(maze,rows,cols);
-        cout << manPosition.x << " " << manPosition.y << "\n";
-        cout << firePositions[0].x << " "<< firePositions[0].y <<"\n";
-        for (int i = 0; i < exitNum;i++){
-            cout << exitPoints[i].exit.x << " " << exitPoints[i].exit.y << "\n";
+        
+        
+        // считаем количество ходов на выход
+        for(int i = 0; i < exitNum; i++){
+            int manWayLength = BFS(maze,manPosition,exitPoints[i].exit,rows,cols);
+            if (exitPoints[i].manStepCount < manWayLength) exitPoints[i].manStepCount = manWayLength;
         }
+        for(int exitN = 0; exitN < exitNum; exitN++){
+            for (int fireN = 0;fireN < fireNum;fireN++){
+                int fireWayLength = BFS(maze,firePositions[fireN],exitPoints[exitN].exit,rows,cols);
+                if (exitPoints[exitN].fireStepCount < fireWayLength) exitPoints[exitN].manStepCount = fireWayLength;
+            } 
+        }
+
+
+        // находим из всех выходов самый короткий и переписываем в число , число изначально -1
+        // переписываем число в ответ 
+        
+
+
     } 
+    // петля которая выводит ответы ечли -1 выписывает нет 
+    
 }
     
