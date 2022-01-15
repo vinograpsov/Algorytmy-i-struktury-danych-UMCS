a5ec693024a2eb5cdfa749c993c271bb82c6266d
Kiryl <kirvinpost@gmail.com>
2022-01-15 13:45:25 +0300

    working algorithm but with mistake

--- Sejf/lesson_try.cpp
+++ Sejf/lesson_try.cpp
@@ -31,7 +31,7 @@ void printArr(char** arr,int n,int k){
 }
 
 
-void lineSort (char** arr,int n, int k){
+void lineSort (char** arr,int n, int k,int m){
     
     char** tempArr = new char*[n];
     for(int i = 0; i < n; ++i) tempArr[i] = new char[k];
@@ -42,24 +42,52 @@ void lineSort (char** arr,int n, int k){
         
         int numX = 0 ,numO = 0;
         for (int j = n - 1;j >= 0; j--){
-            char temp = arr[j][i];
-            if (temp == 'X') numX += 1;
+            if (arr[j][i] == 'X') numX += 1;
             else numO += 1;
         }
         numX += numO;
 
-        cout << numX << " " <<numO << "\n"; 
+        for (int j = n - 1;j >= 0; j--){
+            if (arr[j][i] == 'X'){
+                tempArr[numX -1] = arr[j];
+                numX -=1;
+            }
+            else
+            {
+                tempArr[numO -1] = arr[j];
+                numO -=1;
+            }
+            
+            
+        }
+        // cout <<"-----------------\n";
+        // printArr(tempArr,n,k);
+        // cout <<"-----------------\n" ;
+        // cout << numX << " " <<numO << "\n"; 
+        for(int j = 0; j < n;j++){
+            arr[j] = tempArr[j];
+        }
+        
+        
     
     }
 
-
-    tempArr = arr;
-    printArr(tempArr,n,k);
+    for (int i = n - 1;i >= n - m;i--){
+        for (int j = 0; j < k;j++){
+            cout << arr[i][j];
+        }
+        cout << "\n";
+    }
+    // tempArr = arr;
+    // printArr(tempArr,n,k);
 }
 
 
 
 int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
     int n,k,m;
     cin >> n >> k >> m;
     char** inputArr = new char*[n];
@@ -73,14 +101,14 @@ int main(){
     }
 
 
-    int* tempArr = new int[n];
-    for (int i = 0; i < n; i++){
-        tempArr[i] = coutHardes(inputArr[i],k);
-    }
-    for (int i = 0; i < n;i++){
-        cout << tempArr[i] << " ";
-    }
+    // int* tempArr = new int[n];
+    // for (int i = 0; i < n; i++){
+    //     tempArr[i] = coutHardes(inputArr[i],k);
+    // }
+    // for (int i = 0; i < n;i++){
+    //     cout << tempArr[i] << " ";
+    // }
 
 
-    lineSort(inputArr,n,k);
+    lineSort(inputArr,n,k,m);
 }
